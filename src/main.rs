7f7f0d1fc74a813e1d97c81eedaa8b359c27b3ad
enpitsulin <enpitsulin@gmail.com>
2022-05-10 11:15:29 +0800

    entry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufReader, Read},
+};
+
+mod interpreter;
+
+fn read_file(path: &String) -> String {
+    let mut code_buffer = String::new();
+
+    let file = match File::open(path) {
+        Err(reason) => {
+            panic!("Could not open {}:{}", path, reason)
+        }
+        Ok(file) => file,
+    };
+
+    let mut fin = BufReader::new(file);
+    fin.read_to_string(&mut code_buffer).unwrap();
+    code_buffer
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    interpreter::interpreter(&read_file(&args[1]))
 }
